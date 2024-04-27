ef4fb887faff6e9571b605c5fcf15236f3497538
Peter Varga <wmobil@gmail.com>
2024-04-27 10:23:12 -0700

    Remove handling from TextMessageModule

--- src/modules/TextMessageModule.cpp
+++ src/modules/TextMessageModule.cpp
@@ -14,23 +14,6 @@ ProcessMessage TextMessageModule::handleReceived(const meshtastic_MeshPacket &mp
     LOG_INFO("Received text msg from=0x%0x, id=0x%x, msg=%.*s\n", mp.from, mp.id, p.payload.size, p.payload.bytes);
 #endif
 
-    String message(p.payload.bytes, p.payload.size);
-    LOG_INFO("\nCrankk message received: %s\n", message);
-
-    char nodeIdHex[9];
-    sprintf(nodeIdHex, "%08x", mp.from);
-    String nodeId = String(nodeIdHex);
-    LOG_INFO("\nFrom node id: %s\n", nodeId);
-    if (message == "CR24" && nodeId != "0") {
-        LOG_INFO("\nCreating BlockchainHandler with public key: %s\n", moduleConfig.wallet.public_key);
-        std::unique_ptr<BlockchainHandler> blockchainHandler(
-            new BlockchainHandler(moduleConfig.wallet.public_key, moduleConfig.wallet.private_key));
-
-        String packetId = String(mp.id, HEX);
-        String command = "(free.mesh03.add-received-with-chain \"" + nodeId + "\" \"" + packetId + "\" \"19\")";
-        blockchainHandler->executeBlockchainCommand("send", command);
-    }
-
     // We only store/display messages destined for us.
     // Keep a copy of the most recent text message.
     devicestate.rx_text_message = mp;
